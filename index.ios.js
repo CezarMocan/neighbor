3b1f2b71cbda9339f4d974aa462789f92419c9a3
Cezar Mocan <mocancezar@gmail.com>
2017-02-20 16:08:56 -0800

    MapView working

--- index.ios.js
+++ index.ios.js
@@ -12,41 +12,56 @@ import {
   View
 } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 export default class neighbor extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    }
+    this.onRegionChange = this.onRegionChange.bind(this)
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
   render() {
+    console.log('render')
+    console.log(this.state.region)
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <MapView
+        style={styles.map}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
 
